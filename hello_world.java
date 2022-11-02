e24ef2e7042a09cbfc3bb7202af119c5adf7f13b
INT\daniel.soler <danisoler72@gmail.com>
2022-11-02 12:50:23 +0100

    Add new files

---
+++ hello_world.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class hello_world {
+	
+	public static void main(String[] args) {
+		System.out.print("Hello world");
+		int num = 8;
+		num++;
+		System.out.print("Value is: "+num);
+	}
+
+}
